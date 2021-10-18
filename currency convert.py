dd6700ec6924909f5796c3bc05ce33c6d0553ec4
Budu0101 <60315319+Budu0101@users.noreply.github.com>
2021-10-18 21:06:07 +0300

    Added More Currencies and Remove Unnecessary If Statement

--- currency convert.py
+++ currency convert.py
@@ -2,17 +2,14 @@
 currencies = {
   "USD": 0.73,
   "EUR": 0.85,
-  "JPY": 0.0064
+  "JPY": 0.0064,
+  "CAD": 0.59,
+  "CHF": 0.79,
+  "RON": 0.17,
+  "AUD": 0.54
 }
 
 
 type_currency = input("What currency do you want to convert to pounds? (USD/EUR/JPY):  ")
 amount = int(input("Enter the amount: "))
-if type_currency == "USD":
-   print(f"Amount in pounds: {int(amount) * currencies[type_currency]}")
-elif type_currency == "EUR":
-   print(f"Amount in pounds: {int(amount) * currencies[type_currency]}")
-elif type_currency == "USD":
-   print(f"Amount in pounds: {int(amount) * currencies[type_currency]}")
-else:
-   quit()
+print(f"Amount in pounds: {int(amount) * currencies[type_currency]}")
